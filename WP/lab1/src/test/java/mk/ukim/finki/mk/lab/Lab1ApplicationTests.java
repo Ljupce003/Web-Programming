dc2f264019c9fe4ed8d373b4ba8b62289c077a70
Ljupce003 <Ljupcebrusli1@gmail.com>
2025-06-06 23:23:11 +0200

    Mini changes
    /

--- WP/lab1/src/test/java/mk/ukim/finki/mk/lab/Lab1ApplicationTests.java
+++ WP/lab1/src/test/java/mk/ukim/finki/mk/lab/Lab1ApplicationTests.java
@@ -1,13 +1,32 @@
 package mk.ukim.finki.mk.lab;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Lab1ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    public static WebDriver driver;
+    public static String baseUrl;
+
+//    @BeforeEach
+//    public void setUp() throws Exception{
+//        driver = new FirefoxDriver();
+//        baseUrl = "http://127.0.0.1:4000/#/";
+//    }
+//
+//    @Test
+//    void testRegisterUI(){
+//        driver.get(baseUrl);
+//
+//        System.out.println(driver.getCurrentUrl());
+//        System.out.println(driver.getPageSource());
+//        assertTrue
+//
+//
+//    }
 
 }
